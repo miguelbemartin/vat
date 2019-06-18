6af651e143553203b0a76126431703e432c59f49
Miguel Ángel Martín <miguelbemartin@gmail.com>
2019-06-18 18:07:14 +0200

    Add test for Validate function

---
+++ services/validator_test.go
@@ -0,0 +1,42 @@
+package services
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestValidate(t *testing.T) {
+	var tests = []struct {
+		vatNumber     string
+		foundExpected bool
+		errorExpected string
+	}{
+		{
+			vatNumber:     "",
+			errorExpected: "",
+			foundExpected: false,
+		},
+		{
+			vatNumber:     "ES1234123",
+			errorExpected: "",
+			foundExpected: false,
+		},
+	}
+
+	validatorService := NewValidatorService()
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			found, err := validatorService.Validate(test.vatNumber)
+			if found != test.foundExpected {
+				t.Errorf("Test failed: wrong value\nwant: %t\ngot:  %t", test.foundExpected, found)
+			}
+			if err != nil && err.Error() != test.errorExpected {
+				t.Errorf("Test failed: wrong error\nwant: %q\ngot:  %q", test.errorExpected, err.Error())
+			}
+			if err == nil && test.errorExpected != "" {
+				t.Errorf("Test failed: no error\nexpected:  %q", test.errorExpected)
+			}
+		})
+	}
+}
